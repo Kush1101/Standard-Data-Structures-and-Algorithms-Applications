e73ed0728fe1e983c3da7ab7ff30e05453dedf39
Kushagra Bansal <kushagrabansalajmer@gmail.com>
2020-08-11 15:06:36 +0530

    Update and rename sum.py to sum_optimized.py

--- Sliding WIndow Problems/Triplet sum/sum_optimized.py
+++ Sliding WIndow Problems/Triplet sum/sum_optimized.py
@@ -9,15 +9,19 @@ def triple_sum(arr,x):
             while l<r:
                 if arr[l]+arr[r]==temp:
                     print(arr[i],arr[l],arr[r])
-                    break
+                    return True
                 elif arr[l]+arr[r]<temp:
                     l+=1
                 elif arr[l]+arr[r]>temp:
                     r-=1
+    else:
+        return False
                     
 if __name__ == "__main__":
+    start = time.time()
     test_arr = [1, 4, 45, 6, 10, 8] 
     sum = 22
     triple_sum(test_arr,sum)
+    print("Time taken is ",time.time()-start)
     
             
