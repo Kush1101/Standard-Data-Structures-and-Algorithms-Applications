a2da0057faf8321442335d64e4ccc173380b86d5
Kushagra Bansal <kushagrabansalajmer@gmail.com>
2020-08-27 17:18:53 +0530

    Add files via upload

---
+++ O(n^3) problems./min_triple_sum.py
@@ -0,0 +1,49 @@
+"""
+@author: Kush
+"""
+def naive_solution(arr):
+    """
+    We use a triple loop to create all possible triplets and compute the 
+    minimum sum.
+    """
+    import sys, time
+    start = time.time()
+    min_sum = sys.maxsize
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            for k in range(j+1, len(arr)):
+                if arr[i]+arr[j]+arr[k]<min_sum:
+                    min_sum = arr[i]+arr[j]+arr[k]
+                    
+    return (min_sum, time.time()-start)
+
+def optimized_solution(arr):
+    """
+    Here we will compute the sum by calulating the first, second and third minimum
+    in the array. This approach will take O(n) time.
+    """            
+    import sys, time
+    start = time.time()
+    min1 = sys.maxsize
+    min2 = sys.maxsize
+    min3 = sys.maxsize    
+    for i in range(len(arr)):
+        if arr[i]<min1:
+            min3 = min2
+            min2 = min1
+            min1 = arr[i]
+        elif arr[i]<min2:
+            min3 = min2
+            min2 = arr[i]
+        elif arr[i]<min3:
+            min3 = arr[i]
+    return ((min1 + min2 + min3), time.time()-start)     
+           
+if __name__ == '__main__':
+    from random import randint
+    
+    arr = [random.randint(-1000,1000) for i in range(500)]
+    ans1 = naive_solution(arr)
+    ans2 = optimized_solution(arr)
+    print(f"The minimum sum is {ans1[0]} computed in time {ans1[1]} seconds.")
+    print(f"The minimum sum is {ans2[0]} computed in time {ans2[1]} seconds.")
